34f028f4592ee6af823fcadfe622e92e85404660
Jessica Morrison <jnicomorr@gmail.com>
2023-02-25 10:10:27 -0600

    Edit and delete also works for comments

--- pages/comment/edit/[id].js
+++ pages/comment/edit/[id].js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import CommentForm from '../../../components/forms/CommentForm';
+import { getCommentById } from '../../../utils/data/commentData';
 
-export default function edit() {
+export default function EditVideoGameEntry() {
+  const [editCommentItem, setCommentItem] = useState({});
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    getCommentById(id).then(setCommentItem);
+  }, [id]);
+
   return (
-    <div>[id]</div>
+    <div className="create-form" style={{ height: '45rem', padding: '10%' }}>
+      <CommentForm commentObj={editCommentItem} />
+    </div>
   );
 }
